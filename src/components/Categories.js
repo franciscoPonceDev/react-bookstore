765bef940e00ee3c2d45d1dfa5dee8eec158fa0e
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-03-24 16:15:34 -0300

    add button in categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,6 +1,7 @@
 const Categories = () => (
   <section className="categories-section">
     <h1 className="categories-progress">Coming Soon</h1>
+    <button>Check Status</button>
   </section>
 );
 
