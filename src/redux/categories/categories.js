3aad490eaa9e8f28188253b67495f7e5ec4a732e
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-03-29 18:15:56 -0300

    status

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -3,6 +3,7 @@ const CATEGORIES_STATUS = 'CATEGORIES_STATUS';
 export default function categoriesReducer(state = {}, action) {
   switch (action.type) {
     case CATEGORIES_STATUS:
+      console.log(action);
       return 'Under construction';
     default:
       return state;
